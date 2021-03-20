99c31552ae28db9fe7de8ab78bcf46ea81d3c90b
Daniel Milenkovic <flipbug360@gmail.com>
2021-03-20 10:21:17 +0100

    intitial commit

---
+++ _javascript/app.js
@@ -0,0 +1,5 @@
+function toggleNav() {
+  var menu = document.querySelector('.navbar-menu') 
+  menu.classList.toggle('is-active');  
+}
+  
---
+++ winterthurlions.php
@@ -0,0 +1,30 @@
+<?php
+namespace Grav\Theme;
+
+use Grav\Common\Theme;
+
+class Heilungsweg extends Theme
+{
+
+    public function onThemeInitialized()
+    {
+        if ($this->isAdmin()) {
+            $this->active = false;
+            return;
+        }
+
+        $this->enable([
+            'onPageInitialized' => ['onPageInitialized', 0]
+        ]);
+    }
+
+    public function onPageInitialized()
+    {
+        // Redirect to external_url if external page template
+        $template = $this->grav['page']->template();
+        if ($template === 'external' && isset($this->grav['page']->header()->external_url)) {
+            $url = $this->grav['page']->header()->external_url;
+            $this->grav->redirect($url);
+        }
+    }
+}
